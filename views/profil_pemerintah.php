42e27256f04d7621fdd01d67a49c44f63665a053
unknown <zidane.nur004@gmail.com>
2024-10-10 23:35:54 +0700

    logo svg

--- views/profil_pemerintah.php
+++ views/profil_pemerintah.php
@@ -14,6 +14,7 @@ $i = 0;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Input Data Pemerintah</title>
+    <link rel="shortcut icon" href="../asset/e-suara merah.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/profil_pemerintah.css">
 </head>
